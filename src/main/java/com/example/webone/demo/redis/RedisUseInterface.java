f5968b34fd15decd9ac67eb83784c6499ba44a3f
Narnia <software_good@163.com>
2019-02-19 14:19:31 +0800

    增加hystrix

--- src/main/java/com/example/webone/demo/redis/RedisUseInterface.java
+++ src/main/java/com/example/webone/demo/redis/RedisUseInterface.java
@@ -1,8 +1,10 @@
 package com.example.webone.demo.redis;
 
 import com.example.webone.demo.base.ReturnVO;
+import com.example.webone.demo.base.service.FallBackOfJedis;
 import com.example.webone.demo.redis.vo.JedisRequestVO;
 import com.example.webone.demo.redis.vo.JedisReturnVO;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -13,8 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @author lhx
  * @date 2018/12/12
  */
-@FeignClient(name = "redis")
+@FeignClient(name = "redis",fallback = FallBackOfJedis.class)
 public interface RedisUseInterface {
+
     @PostMapping(value = "/jedisStr/jedisForStringSet")
     ReturnVO<JedisReturnVO> jedisForStringSet(@RequestBody JedisRequestVO jedisRequestVO);
 }
