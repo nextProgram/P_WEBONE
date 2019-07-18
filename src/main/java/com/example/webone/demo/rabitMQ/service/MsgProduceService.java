324a6c6d7e32197cb618e18f4213a7ee7da19a4f
Narnia <software_good@163.com>
2019-07-18 16:34:43 +0800

    rabbitmq发送方消息确认

--- src/main/java/com/example/webone/demo/rabitMQ/service/MsgProduceService.java
+++ src/main/java/com/example/webone/demo/rabitMQ/service/MsgProduceService.java
@@ -2,10 +2,9 @@ package com.example.webone.demo.rabitMQ.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 /**
@@ -37,14 +36,25 @@ public class MsgProduceService {
      * 定制amqp模版
      * ConfirmCallback接口用于实现消息发送到RabbitMQ交换器后接收ack回调 ，即消息发送到exchange再ack，即保证消息发送到了broker
      * ReturnCallback接口用于实现消息发送到RabbitMQ交换器，但无相应队列与交换器绑定时的回调，即消息发送不到任何一个队列中则ack
+     * Negative Acknowledgment In exceptional cases when the broker is unable to handle messages successfully, instead of a basic.ack, the broker will send a basic.nack. In this context, fields of the basic.nack have the same meaning as the corresponding ones in basic.ack and the requeue field should be ignored. By nack'ing one or more messages, the broker indicates that it was unable to process the messages and refuses responsibility for them; at that point, the client may choose to re-publish the messages.
+     * After a channel is put into confirm mode, all subsequently published messages will be confirmed or nack'd once. No guarantees are made as to how soon a message is confirmed. No message will be both confirmed and nack'd.
+     * basic.nack will only be delivered if an internal error occurs in the Erlang process responsible for a queue.
+     * In addition to this, Spring AMQP generates a Nack if the connection is closed before an ack is received (again - very rare).
+     * If you want to get notified about the inability to deliver a message to a queue, you must enable publisher returns and set mandatory to true, and the message will be returned to you.
      * @param exchange 交换器名称
      * @param routingKey routingKey
      * @param message 消息
      */
-    public void sendOrder(String exchange,String routingKey,String message){
-        // 消息发送失败返回到队列中, 需要配置 publisher-returns: true
+    public void sendOrder(String exchange,String routingKey,String message,CorrelationData correlationData1){
+        //采用实现接口的方式设置ConfirmCallback
+        //rabbitTemplate.setConfirmCallback(this);
+        //发送消息
+        logger.debug("publisher CallBackConfirm ID: {}",correlationData1.getId());
+        rabbitTemplate.convertAndSend(exchange,routingKey,message,correlationData1);
+        logger.info("----->发送消息成功,exchange:{},routingKey:{},message:{}",exchange,routingKey,message);
+        //消息确认, 需要配置 publisher-confirms: true
+        //消息发送失败则返回, 需要配置 publisher-returns: true
         rabbitTemplate.setMandatory(true);
-        // 消息确认, 需要配置 publisher-confirms: true
         rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
             if (ack) {
                 //发送成功
@@ -59,8 +69,27 @@ public class MsgProduceService {
             String correlationId = message.getMessageProperties().getCorrelationId();
             logger.debug("消息：{} 发送失败, 应答码：{} 原因：{} 交换机: {}  路由键: {}", correlationId, replyCode, replyText, exchange, routingKey);
         });*/
-        //发送消息
-        rabbitTemplate.convertAndSend(exchange,routingKey,message);
-        logger.info("----->发送消息成功,exchange:{},routingKey:{},message:{}",exchange,routingKey,message);
     }
+
+    /**
+     * 发送方确认
+     *  需要实现接口：implements RabbitTemplate.ConfirmCallback
+     *  需要发送之前设置：rabbitTemplate.setConfirmCallback(this)
+     *  该方法等同于上面setConfirmCallback
+     * @param correlationData
+     * @param ack
+     * @param cause
+     */
+  /*  @Override
+    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+        logger.debug("CallBackConfirm ID: {}",correlationData.getId());
+        if(ack) {
+            logger.debug("CallBackConfirm 消息发送成功！");
+        }else {
+            logger.debug("CallBackConfirm 消息发送失败！");
+        }
+        if(cause!=null) {
+            logger.debug("CallBackConfirm Cause: " + cause);
+        }
+    }*/
 }
--- src/test/java/com/example/webone/demo/rabitMQ/service/MsgProduceServiceTest.java
+++ src/test/java/com/example/webone/demo/rabitMQ/service/MsgProduceServiceTest.java
@@ -2,9 +2,12 @@ package com.example.webone.demo.rabitMQ.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
@@ -30,9 +33,13 @@ public class MsgProduceServiceTest {
 
     /**
      * 一个exchange绑定两个queue
+     * 发送10条消息 结果：发送成功 报错：Exception delivering confirm
      */
     @Test
     public void sendOrder() {
-        msgProduceService.sendOrder("myOrder","fruit","fruit message");
+        for(int i = 0;i<5;i++){
+            CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+            msgProduceService.sendOrder("myOrder","fruit","fruit message "+i,correlationData);
+        }
     }
 }
